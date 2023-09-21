5ff62c56e59167ecd2c8a04ca352bf8a716f2176
ThomasvGroen <tgroen452@gmail.com>
2023-09-21 11:27:05 +0200

    Word Puzzle completed

--- Assets/Scripts/Word Puzzle.cs
+++ Assets/Scripts/Word Puzzle.cs
@@ -6,69 +6,98 @@ using TMPro;
 
 public class WordPuzzle : MonoBehaviour
 {
-    [SerializeField] private string Code;
-    [SerializeField] private PlayerMovement PlayerController;
-    [SerializeField] private GameObject bookUI;
+    [SerializeField] private string code;
+    [SerializeField] private PlayerMovement playerController;
+    [SerializeField] private GameObject crateUI;
+    [SerializeField] private GameObject interactionUI;
+    [SerializeField] private GameObject topCrate;
     private GameObject inputText;
 
     private bool canInteract = false;
-    private bool isDoor1Open;
+    private bool codeUsed;
 
     void Start()
     {
-        inputText = bookUI.transform.Find("Input").GetChild(0).Find("Text").gameObject;
+        inputText = crateUI.transform.Find("Input").GetChild(0).Find("Text").gameObject;
     }
     private void Update()
     {
         Keybinds(); // Fires function KeyBinds() 
     }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player")) // Checks if the collider that entered, is the collider of the player
+        {
+            if (codeUsed == false) // Checks if there is any door closed
+            {
+                canInteract = true; // Sets canInteract to true
+                interactionUI.SetActive(true); // Activates the gui
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player")) // Checks if the collider that left, is the collider of the player
+        {
+            canInteract = false; // Sets canInteract to false
+            interactionUI.SetActive(false); // Deactivates the gui
+        }
+    }
+
     private void Keybinds()
     {
         if (Input.GetKeyDown(KeyCode.E) && canInteract) // Checks if the player can interact and is pressing E
         {
-            PlayerController.canLook = false;
-            PlayerController.canWalk = false;
+            playerController.canLook = false;
+            playerController.canWalk = false;
+
+            // Activating the book gui
+            crateUI.SetActive(true);
         }
 
-        // Activating the book gui
-        bookUI.SetActive(true);
+        
     }
     public void _ConfirmButton()
     {
+        Debug.Log("Confirmed");
         string stringInput = inputText.GetComponent<TMP_Text>().text; // Getting the string
-        int openDoor = -1;
+        int openCrate = -1;
 
         stringInput = stringInput.ToLower(); // Lowering the string input
 
         // Setting the correct chars to 0
-        int correctCharsDoor1 = 0;
+        int correctCharCrate = 0;
 
         // Checking if the string is the same
-        if (stringInput.Length == Code.Length + 1 && !isDoor1Open)
+        if (stringInput.Length == code.Length + 1 && !codeUsed)
         {
-            for (int i = 0; i < Code.Length; i++)
+            for (int i = 0; i < code.Length; i++)
             {
-                if (Code[i] == stringInput[i])
+                if (code[i] == stringInput[i])
                 {
-                    correctCharsDoor1 += 1;
+                    Debug.Log(correctCharCrate);
+                    correctCharCrate += 1;
                 }
             }
         }
 
         // Checking if the string is the same
-        if (correctCharsDoor1 == Code.Length)
+        if (correctCharCrate + 1 == code.Length)
         {
-            isDoor1Open = true;
-            openDoor = 1;
+            Debug.Log("Code is correct");
+            codeUsed = true;
+            openCrate = 1;
         }
 
-        if (openDoor != -1) // If the anwser was right
+        if (openCrate != -1) // If the anwser was right
         {
-            bookUI.SetActive(false); // Disabling the bookui
+            crateUI.SetActive(false); // Disabling the bookui
             Cursor.lockState = CursorLockMode.Locked; // Locking the cursor
-
-            PlayerController.canLook = true;
-            PlayerController.canWalk = true;
+            topCrate.SetActive(false);
+            playerController.canLook = true;
+            playerController.canWalk = true;
 
             // Opening the correct door
            // crawlDoorHandler.OpenDoor(openDoor);
@@ -77,12 +106,12 @@ public class WordPuzzle : MonoBehaviour
     }
     public void _ExitUI()
     {
-        bookUI.gameObject.SetActive(false); // Disabling the bookui
+        crateUI.gameObject.SetActive(false); // Disabling the bookui
         Cursor.lockState = CursorLockMode.Locked; // Locking the cursor
 
        
 
-        PlayerController.canLook = true;
-        PlayerController.canWalk = true;
+        playerController.canLook = true;
+        playerController.canWalk = true;
     }
 }
